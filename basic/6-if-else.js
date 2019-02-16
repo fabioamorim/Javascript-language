73b8e0fa0e88c6567aef3f763ae0a1174882131a
Fabio Batista do Amorim <batista.amorim@hotmail.com>
2019-02-16 13:27:06 -0200

    Add new file

---
+++ basic/6-if-else.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Javascript supports the following forms of if..else statement - 
+ * 
+ * - if statement 
+ * - if..else statement
+ * - if..sele if statement
+ * 
+ */
+
+ let n1 = 10;
+
+ //if
+ if(n1 == 0 ){
+     //code
+ }
+
+ //if else
+ if(n1 == 0){
+    //code
+ }else{
+     //code
+ }
+
+ //if else if
+
+ if(n1 == 0){
+    //code
+ }else if (n1 == 1) {
+    //code   
+ } else {
+     //code
+ }
