171eb1b1f79d1785f2c866053b01531cde6757fb
Fabio Batista do Amorim <batista.amorim@hotmail.com>
2019-02-15 22:34:14 -0200

    Add folder basic

---
+++ basic/datatypes.js
@@ -0,0 +1,23 @@
+/**
+ * javascript data types
+ * 
+ * Numbers
+ * Strings
+ * Boolean
+ * 
+*/
+
+let n1 = 10;
+let n2 = 3.25;
+let text = "This text string";
+let temp = true;
+
+console.log(typeof n1, typeof n2);
+//output: number number
+
+console.log(typeof text);
+//string
+
+console.log(typeof temp);
+//boolean
+
