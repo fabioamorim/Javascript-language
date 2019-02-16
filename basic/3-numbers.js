282a0c64d08783391358822f163389dfd4f60e0f
Fabio Batista do Amorim <batista.amorim@hotmail.com>
2019-02-16 09:39:35 -0200

    Add new file

---
+++ basic/3-numbers.js
@@ -0,0 +1,22 @@
+
+// Tipo number
+
+let n1 = 1;
+let n2 = Number('1.1');
+
+console.log(n1, n2);
+
+// Number.isInteger verifica se o número é um inteiro
+
+console.log(Number.isInteger(n1)); // true
+console.log(Number.isInteger(n2)); // false
+
+let resultado = 1500 / 654512 ;
+
+console.log(resultado);
+
+console.log(resultado.toFixed(4));
+console.log(n1.toString(2));
+
+console.log(typeof resultado);
+console.log(typeof Number);
