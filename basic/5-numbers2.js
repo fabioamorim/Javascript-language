45a95d270e39013e652a6453de7fdfa69b661356
Fabio Batista do Amorim <batista.amorim@hotmail.com>
2019-02-16 13:17:46 -0200

    Add new file

---
+++ basic/5-numbers2.js
@@ -0,0 +1,14 @@
+//Some outputs of Numbers
+
+console.log(10 / 0); // do not raise an Exception
+//output: Infinity
+
+console.log("10" / 2);
+//output: 5
+
+console.log("10,0" / 2);
+//output: NaN
+
+console.log("10" + 2); // + has the function of concatenating
+//output: 102 
+
