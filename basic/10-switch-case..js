cd46e947b780dc835a3ec3f4a01950b6893504b8
Fabio Batista do Amorim <batista.amorim@hotmail.com>
2019-02-18 20:56:04 -0300

    Add nnew file

---
+++ basic/10-switch-case..js
@@ -0,0 +1,19 @@
+let value = 5;
+
+switch (value) {
+    case 1:
+        console.log('Caso 1');
+        break;
+    case 2:
+        console.log('Caso 2');
+        break;
+    case 3:
+        console.log('Caso 3');
+        break;
+    case 4:
+        console.log('Caso 4');
+        break;
+    default:
+        console.log('Nenhum caso encontrado!');
+        break;
+}
