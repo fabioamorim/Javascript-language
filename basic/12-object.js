edf957bf44d17254aa2f0966c5b41a378d679a27
Fabio Batista do Amorim <batista.amorim@hotmail.com>
2019-05-02 10:09:49 -0300

    Add new file 12-object

---
+++ basic/12-object.js
@@ -0,0 +1,18 @@
+/*
+    Um objeto é uma coleção de par de chave e valor.
+*/
+
+const produto = {}; //sintaxe básica 
+
+produto.descricao = 'Notebook usado'; // implicitamente estou criando e atribuindo valor para um atributo
+
+produto.preco = 5000.99;
+
+console.log(produto);
+
+const produto2 = {
+    nome: 'Violão novo',
+    preco: 500.00
+}
+
+console.log(produto2);
