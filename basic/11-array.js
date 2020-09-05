41156e5660937e33eecc629da8ef0d9c43c4d045
Fábio Batista do Amorim <batista.amorim@hotmail.com>
2020-09-05 15:58:28 -0300

    Add array file

---
+++ basic/11-array.js
@@ -0,0 +1,11 @@
+/*
+  Array.
+*/
+
+var array1 = [10,20,30];
+var array2 = ['a', 'b', 'c', 'd'];
+var array3 = ['foo', 10, 'a', 1.23];
+
+console.log(array1); // [ 10, 20, 30 ]
+console.log(array2); // [ 'a', 'b', 'c', 'd' ]
+console.log(array3); // [ 'foo', 10, 'a', 1.23 ]
